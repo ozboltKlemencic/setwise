c4f9ddb10d88b35763545036cb6a640706f2a54f
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2026-02-04 21:03:32 +0100

    fff

--- components/bento-grid-section.tsx
+++ components/bento-grid-section.tsx
@@ -111,11 +111,11 @@ const WorkoutNotification = ({ name, description, icon, color, time, highlight }
                 "transition-all duration-200 ease-in-out ",
 
                 "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
-                highlight && "border border-blue-500"
+                highlight && "border border-blue-500 hover:bg-linear-to-b hover:from-blue-500/5 hover:-translate-y-px hover:to-white transition-all duration-300 "
             )}
         >
             {highlight && (
-                <div className="absolute -top-2.5 right-10 border border-blue-500 bg-white text-neutral-800 text-[9px] font-bold px-2 py-0.5 z-10 font-sans tracking-wide rounded-full shadow-sm antialiased transition-all duration-200 ease-in-out">
+                <div className="absolute -top-2.5 right-8 border border-blue-500 bg-white text-neutral-800 text-[9px] font-bold px-2 py-0.5 z-10 font-sans tracking-wide rounded-full shadow-sm antialiased transition-all duration-200 ease-in-out">
                     Klikni
                 </div>
             )}
