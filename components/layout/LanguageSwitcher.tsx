868f38aa1ade6fe739c0e56ea9887958d8d28b86
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2026-02-07 18:11:42 +0100

    ggd

--- components/layout/LanguageSwitcher.tsx
+++ components/layout/LanguageSwitcher.tsx
@@ -29,7 +29,7 @@ export default function LanguageSwitcher() {
         <a
             href={otherLocalePath}
             onClick={handleClick}
-            className="py-2 px-2.5 duration-200 rounded-full text-xs font-medium transition-all hover:to-neutral-100 hover:from-neutral-200/80 text-gray-600 hover:text-neutral-800 border hover:border-neutral-300 border-neutral-200/60 shadow-xs backdrop-blur-md bg-linear-to-tr from-neutral-100 to-neutral-200/80 hover:ring-2 hover:ring-offset-1 hover:ring-neutral-200/80 focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-neutral-200/80 focus:scale-95"
+            className="py-2 px-2.5 duration-200 rounded-full text-xs font-medium transition-all hover:to-neutral-100 hover:from-neutral-200/80 text-gray-600 hover:text-neutral-800 border hover:border-neutral-300 border-neutral-200/60 shadow-xs backdrop-blur-md bg-linear-to-tr from-neutral-100 to-neutral-200/80 hover:ring-2 hover:ring-offset-1 hover:ring-neutral-200/80 focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-neutral-200/80 focus:scale-95 transform-gpu will-change-transform"
         >
             {currentLocale}
         </a>
