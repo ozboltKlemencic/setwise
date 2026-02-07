c13e8e1fd66fc831c0f3ec61cd5e735a7a05fc31
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2026-02-07 20:05:12 +0100

    d

--- components/footer-section.tsx
+++ components/footer-section.tsx
@@ -84,12 +84,12 @@ export default function FooterSection() {
             </div>
             <div className="flex items-center gap-x-3">
               <a href="#" className="  ">
-                <Instagram className="size-4 font-thin text-neutral-500  hover:text-neutral-800 transition-colors duration-200" />
+                <Instagram className="md:size-4 size-3 font-thin text-neutral-500  hover:text-neutral-800 transition-colors duration-200" />
               </a>
               <a href="#" className="  h">
-                <Linkedin className="size-4 font-thin text-neutral-500 hover:text-neutral-800 transition-colors duration-200" />
+                <Linkedin className="md:size-4 size-3 font-thin text-neutral-500 hover:text-neutral-800 transition-colors duration-200" />
               </a>
-              <a href="#" className=" size-4 text-neutral-500 hover:text-neutral-800 flex items-center justify-center">
+              <a href="#" className=" md:size-4 size-3 text-neutral-500 hover:text-neutral-800 flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-tiktok" viewBox="0 0 16 16">
                   <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
                 </svg>
