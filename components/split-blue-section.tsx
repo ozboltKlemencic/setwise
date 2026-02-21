ec29a38e779d3b07e3f31fdff3fc0a3d4b682675
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2026-02-21 21:53:25 +0100

    pfp update

--- components/split-blue-section.tsx
+++ components/split-blue-section.tsx
@@ -29,8 +29,8 @@ export default function SplitBlueSection() {
 
     const teamMembers: TeamMember[] = translatedMembers.map((member) => ({
         ...member,
-        avatarUrl: member.name === "Jernej Peternel" ? "/team/2.png" : "/team/1.png",
-        miniAvatarUrl: member.name === "Jernej Peternel" ? "/team/2.png" : "/team/1.png",
+        avatarUrl: member.name === "Jernej Peternel" ? "/team/2.png" : "/team/ozbolt-klemencic.png",
+        miniAvatarUrl: member.name === "Jernej Peternel" ? "/team/2.png" : "/team/ozbolt-klemencic.png",
         status: "Online"
     }))
 
