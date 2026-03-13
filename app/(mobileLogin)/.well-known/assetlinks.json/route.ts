13af021be20dbb2fd2a7d0cf155a0a798923a848
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2026-03-13 15:56:35 +0100

    update assetslinks json

--- app/(mobileLogin)/.well-known/assetlinks.json/route.ts
+++ app/(mobileLogin)/.well-known/assetlinks.json/route.ts
@@ -7,7 +7,8 @@ export async function GET() {
         package_name: "com.jernejpeternel.myfit",
         sha256_cert_fingerprints: [
           "F9:18:C9:9E:82:34:3A:F8:B6:EA:7A:D9:C3:20:F7:D5:10:E1:D4:3E:AB:59:89:28:57:A8:01:A3:E8:F2:E1:DE",
-        ],
+          "9F:97:64:60:44:F0:CA:4C:8A:36:4A:19:8A:1F:84:4D:EF:95:9B:79:3E:27:F0:DE:7C:77:90:F4:04:B9:F4:1B"
+        ]
       },
     },
   ];
